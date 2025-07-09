2e03068801c5c5627a79fdb50724438cc5c3617b
Kulwant Bawa <44430853+kulwantablu@users.noreply.github.com>
2025-07-09 10:03:30 +0530

    Update db.js

--- src/db.js
+++ src/db.js
@@ -6,6 +6,7 @@ export async function connectDB() {
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_NAME,
+    port:process.env.DB_MYSQL_PORT,
   });
   return connection;
 }
